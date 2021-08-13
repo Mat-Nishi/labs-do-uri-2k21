bee8e0e3764381e56a69b6195e678fae2386c5a3
Mat-Nishi <73362075+Mat-Nishi@users.noreply.github.com>
2021-08-13 19:47:11 -0300

    Update lab 6.py

--- lab 6.py
+++ lab 6.py
@@ -1,22 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 13 19:44:10 2021
+
+@author: Mateus
+"""
+
+# Conjunto de funções para uma calculadora.
+
+
 def soma(x, y):
     resultado = x+y
-
     return resultado
+
 
 def subtracao(x, y):
     resultado = x-y
-
     return resultado
+
 
 def multiplicacao(x, y):
     resultado = x*y
-
     return resultado
+
 
 def divisao(x, y):
     resultado = x/y if y else None
-
     return resultado
+
 
 def exponenciacao(x, y):
     if (x == 0 and y < 0) or type(y) != int:
@@ -26,11 +36,13 @@ def exponenciacao(x, y):
 
     return resultado
 
+
 def somasRec(N, denominador, ciclo):
     if N == ciclo:
         return 0
     else:
         return (((-1)**N)/denominador) + somasRec(N+1, denominador+2, ciclo)
+
 
 def pi(N):
     if N < 0 or type(N) != int:
@@ -40,13 +52,15 @@ def pi(N):
     else:
         return float(4*(1 + somasRec(1, 3, N)))
 
+
 def fatorial(x):
-    if x < 0:
+    if x < 0 or type(x) != int:
         return None
     elif x == 0 or x == 1:
         return 1
     else:
         return x*fatorial(x-1)
+
 
 def permutacao(n, p):
     if n >= 0 and type(n) == int and p <= n and p >= 0 and type(p) == int:
@@ -56,6 +70,7 @@ def permutacao(n, p):
 
     return int(resultado)
 
+
 def combinacao(n, p):
     if n >= 0 and type(n) == int and p <= n and p >= 0 and type(p) == int:
         resultado = fatorial(n)/(fatorial(n-p)*fatorial(p))
@@ -64,6 +79,7 @@ def combinacao(n, p):
 
     return int(resultado)
 
+
 def somatorio(i, f):
     if type(i) == int and type(f) == int:
         resultado = sum(range(i, f+1))
@@ -71,6 +87,7 @@ def somatorio(i, f):
         resultado = None
 
     return resultado
+
 
 def exp(x):
     soma = 0
@@ -81,6 +98,7 @@ def exp(x):
         soma = int(soma)
 
     return soma
+
 
 def ln(x):
     if x > 0:
@@ -95,6 +113,7 @@ def ln(x):
 
     return resultado
 
+
 def superExponenciacao(x, y):
     if x > 0:
         resultado = exp(y*ln(x))
@@ -103,8 +122,10 @@ def superExponenciacao(x, y):
 
     return resultado
 
+
 def raizQuadrada(x):
     if x >= 0:
+        # resultado = x**(0.5) ou
         resultado = superExponenciacao(x, 0.5)
     else:
         resultado = None
